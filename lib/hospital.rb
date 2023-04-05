e4c621e4f09aea921a84641bb62195e520d8eedb
Alexander <alexander@presber.net>
2023-04-05 14:07:29 +0200

    build a hospital

---
+++ lib/hospital.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hospital/version"
+
+module Hospital
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hospital/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Hospital
+  VERSION = "0.1.0"
+end
